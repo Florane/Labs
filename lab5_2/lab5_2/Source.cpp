70299dfd0fcaa169638ac160bd197d80845ee6f7
Florane <qwerty.qa912@gmail.com>
2020-09-25 16:56:59 +0300

    finished lab 5

---
+++ lab5_2/lab5_2/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+    double x;
+    printf_s("Enter x: ");
+    scanf_s("%lf", &x);
+    double result;
+
+    if(x<0) {result = (x*=x)*x;}
+    else {if(x <= 10) {result = 5*sin(x);}
+    else {if(x > 10) {result = 1/tan(x);}}}
+
+    printf_s("Result: %lf\n", result);
+}
