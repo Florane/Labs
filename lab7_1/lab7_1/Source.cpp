b22c91065bec4be43d30b3df6c241e8e73a05fdd
Florane <qwerty.qa912@gmail.com>
2020-09-26 12:21:00 +0300

    Finished lab 7

---
+++ lab7_1/lab7_1/Source.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int log(int a, int b)
+{
+    int c = 0;
+    while (b != 1)
+    {
+        if(b%a != 0) //power is not integer
+            return -1;
+        b /= a;
+        c++;
+    }
+    return c;
+}
+
+int main()
+{
+    int a, b;
+    printf_s("Enter 2 numbers: ");
+    scanf_s("%d%d",&a,&b);
+
+    int result = log(a,b);
+    if(result == -1)
+        printf_s("Impossible");
+    else
+        printf_s("Result: %d",result);
+
+    printf_s("\n");
+}
---
+++ lab7_2/lab7_2/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int number;
+    printf_s("Enter number: ");
+    scanf_s("%d", &number);
+
+    int result = 0;
+    int i = 0;
+    while(number > 0)
+    {
+        result += number%10;
+        number /= 10;
+        i++;
+    }
+    printf_s("Result = %lf", double(result)/i);
+}
