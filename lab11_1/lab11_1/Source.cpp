530890f7e01187a9aa3e5cb99b134ac5a1afc95a
Florane <qwerty.qa912@gmail.com>
2020-09-27 09:45:56 +0300

    Finished lab 11

---
+++ lab11_1/lab11_1/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int n; double x;
+    printf_s("Enter n: ");
+    scanf_s("%d", &n);
+    printf_s("Enter x: ");
+    scanf_s("%lf", &x);
+
+    double result = 0;
+    for(int i = 0;i < n;i++)
+    {
+        double step = i%2==0 ? -1 : 1;
+        step *= 2*x;
+        printf_s("%d: %lf\n",i,step);
+        result += step;
+    }
+    printf_s("Result: %lf\n",result);
+}
---
+++ lab11_2/lab11_2/Source.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <math.h>
+
+double pow(int a, int b)
+{
+    double result = 1;
+    for(int i = 0;i < b;i++)
+    {
+        result*=a;
+    }
+    return result;
+}
+
+double factorial(int n)
+{
+    double result = 1;
+    for(int i = 1;i <= n;i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+
+int main()
+{
+    int x; double e;
+    printf_s("Enter x: ");
+    scanf_s("%d",&x);
+    printf_s("Enter approx: ");
+    scanf_s("%lf",&e);
+
+    double result = 0;
+    double step = e+1;
+    for(int i = 0;fabs(step)>e;i++)
+    {
+        step = i%2==0 ? -1 : 1;
+        step *= (factorial(x+1))/(pow(i+1,i+1));
+        printf_s("%d: %lf\n",i,step);
+        result += step;
+    }
+    printf_s("Result: %lf\n",result);
+}
