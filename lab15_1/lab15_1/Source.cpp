4a2b2c5535baf2391b7e62f2384fc396e56f5638
Florane <qwerty.qa912@gmail.com>
2020-10-03 12:45:50 +0300

    Finished lab 15

---
+++ lab15_1/lab15_1/Source.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <conio.h>
+
+struct Student
+{
+    int grade[3];
+};
+
+double mean(Student student)
+{
+    return (student.grade[0]+student.grade[1]+student.grade[2])/3.0;
+}
+
+void body(Student* num,int size)
+{
+    double max = -1;int pos;
+    for(int i = 0;i < size;i++)
+    {
+        if(mean(num[i]) > max)
+        {
+            pos = i;
+            max = mean(num[i]);
+        }
+    }
+    printf_s("Student %d has the biggest mean grade: %lf\n",pos+1,max);
+}
+
+void inputManual()
+{
+    int size;
+    printf_s("Enter a count of students: ");
+    scanf_s("%d",&size);
+
+    Student* num = (Student*) malloc(size*sizeof(Student));
+    for(int i = 0;i < size;i++)
+    {
+        printf_s("Enter 3 grades for student %d: ",i+1);
+        scanf_s("%d%d%d",&num[i].grade[0],&num[i].grade[1],&num[i].grade[2]);
+    }
+    body(num,size);
+}
+
+void inputFromFile()
+{
+    FILE* file;
+    fopen_s(&file, "file.txt", "r");
+    Student num[85]; int i = 0, test;
+    while(!feof(file))
+    {
+        Student buffer;
+        test = fscanf_s(file,"%d%d%d",&buffer.grade[0],&buffer.grade[1],&buffer.grade[2]);
+        if(test != EOF)
+        {
+            num[i] = buffer;
+            i++;
+        }
+    }
+    fclose(file);
+    body(&num[0],i);
+}
+
+int main()
+{
+    printf_s("Select: \n1: Enter manually\n2: Enter from file\n");
+    char c;
+    do {
+        c = _getch();
+    } while(c<'1'||c>'2');
+
+    switch(c)
+    {
+        case '1': inputManual(); break;
+        case '2': inputFromFile(); break;
+    }
+
+    return 0;
+}
