332afa5b8c4bd498e700e8aaf1675f2e7ed1f9d2
Florane <qwerty.qa912@gmail.com>
2020-11-28 18:42:14 +0300

    Testing headers

--- lab13_1/lab13_1/Source.cpp
+++ lab13_1/lab13_1/Source.cpp
@@ -1,36 +1,12 @@
 #include <iostream>
+
 #include <conio.h>
 #include <time.h>
 #include <math.h>
 
+#include "lab13.h"
+
 #define SIZE 256
-
-void result(int* num, int size)
-{
-    printf_s("Start:");
-    for(int i = 0;i < size;i++)
-        printf_s(" %d",num[i]);
-    printf_s("\n");
-
-    double result = 1;
-    for(int i = 0;i < size;i++)
-    {
-        result *= num[i];
-    }
-    result = pow(result,1.0/size);
-    printf_s("Result(1): %lf\n",result);
-
-    for(int i = 0;i < size-1;i+=2)
-    {
-        num[i] = num[i]^num[i+1];
-        num[i+1] = num[i+1]^num[i];
-        num[i] = num[i]^num[i+1];
-    }
-    printf_s("Result(2):");
-    for(int i = 0;i < size;i++)
-        printf_s(" %d",num[i]);
-    printf_s("\n");
-}
 
 void manual(int size)
 {
